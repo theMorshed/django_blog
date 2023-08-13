62dd9102506ded563598043651b2ddd47312a3a3
theMorshed <themorshedctg@gmail.com>
2023-08-13 13:54:03 +0600

    create blog model and add it admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class BlogModel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.ForeignKey(
+        'auth.user',
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_details", kwargs={"pk": self.pk})
+    
