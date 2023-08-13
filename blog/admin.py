584c5f13e448e132f1613e04a870f5a6175b2ddf
theMorshed <themorshedctg@gmail.com>
2023-08-13 14:01:33 +0600

    register blog model into admin and create super user

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import BlogModel
 
 # Register your models here.
+@admin.register(BlogModel)
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'description']
